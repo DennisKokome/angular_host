27db1b3d8606b4cd00c7879fbd014ee5a898680d
Oaitse Kokome <okokome@bitri.co.bw>
2021-06-06 18:51:47 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { IntroComponent } from './component/intro/intro.component';
+import { OtherComponent } from './component/other/other.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NavComponent,
+    IntroComponent,
+    OtherComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
